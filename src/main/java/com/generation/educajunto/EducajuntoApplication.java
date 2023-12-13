bfdbb2716d78fb45cc85d4a14387d65408d252ed
Victoria_Moraes <victoria.candido.mm@gmail.com>
2023-12-13 09:50:43 -0300

    Criar o Projeto Spring

---
+++ src/main/java/com/generation/educajunto/EducajuntoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.educajunto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducajuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducajuntoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/educajunto/EducajuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.educajunto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducajuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
