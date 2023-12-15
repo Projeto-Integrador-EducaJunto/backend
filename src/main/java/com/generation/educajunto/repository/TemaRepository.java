950ffdd89805cda6366859ab136ad3ac4c8e9e1c
Deborah <deborag.g17@gmail.com>
2023-12-15 10:34:32 -0300

    Criação método getbyid/getAll

--- src/main/java/com/generation/educajunto/repository/TemaRepository.java
+++ src/main/java/com/generation/educajunto/repository/TemaRepository.java
@@ -2,11 +2,12 @@ package com.generation.educajunto.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.generation.educajunto.model.Tema;
 
-public interface TemaRepository {
+public interface TemaRepository extends JpaRepository<Tema, Long> {
 
 	public List<Tema> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 	
