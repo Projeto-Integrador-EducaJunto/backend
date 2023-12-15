52bb8a6879085bef2e961ca3379d5f29ef9cddef
Deborah <deborag.g17@gmail.com>
2023-12-15 10:41:43 -0300

    Criação método getbyid/getAll

--- src/main/java/com/generation/educajunto/controller/TemaController.java
+++ src/main/java/com/generation/educajunto/controller/TemaController.java
@@ -3,6 +3,7 @@ package com.generation.educajunto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,18 +18,29 @@ import com.generation.educajunto.repository.TemaRepository;
 @RequestMapping("/temas")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TemaController {
-	
+
 	@Autowired
 	private TemaRepository temaRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Tema>> getAll() {
+		return ResponseEntity.ok(temaRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> getById(@PathVariable Long id) {
+		return temaRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
 	@GetMapping("/nome/{nome}")
 	public ResponseEntity<List<Tema>> getByNome(@PathVariable String nome) {
 		return ResponseEntity.ok(temaRepository.findAllByNomeContainingIgnoreCase(nome));
 	}
-	
+
 	@GetMapping("/descricao/{descricao}")
 	public ResponseEntity<List<Tema>> getByDescricao(@PathVariable String descricao) {
 		return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
-	
+
 }
