98da1efc622589b813616c06008eddaa66dca6e6
Gabriel Sponda Freitas Bettarello <sponda.gabriel@aluno.ufabc.edu.br>
2023-12-18 10:13:12 -0300

    Ajuste em PostagemRepository e finalizando a PostagemController.

--- src/main/java/com/generation/educajunto/repository/PostagemRepository.java
+++ src/main/java/com/generation/educajunto/repository/PostagemRepository.java
@@ -1,6 +1,13 @@
 package com.generation.educajunto.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.educajunto.model.Postagem;
+
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
-	List<Postagem> findAllByConteudoContainingIgnoreCase(@Param ("conteudo")String conteudo);
+	List<Postagem> findAllByConteudoContainingIgnoreCase(@Param("conteudo")String conteudo);
 }
