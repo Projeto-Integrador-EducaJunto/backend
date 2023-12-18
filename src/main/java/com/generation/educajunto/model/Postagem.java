ca34ac402d14ddafdc7d4ffe95de2b017f534885
Bruno Cesar <cesarbruno53@gmail.com>
2023-12-18 10:21:41 -0300

    criação da model usuario e postagem

--- src/main/java/com/generation/educajunto/model/Postagem.java
+++ src/main/java/com/generation/educajunto/model/Postagem.java
@@ -1,5 +1,82 @@
 package com.generation.educajunto.model;
 
-public class Postagem {
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_postagens") 
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	
+	@NotBlank(message = " O atributo conteudo é obrigatório.")
+	@Column(columnDefinition = "TEXT")
+	private String conteudo;
+	
+	
+	private String anexo;
+	
+	
+	@NotNull(message = " O atributo estado é obrigatório.")
+	private Integer estado;
+	
+	@UpdateTimestamp
+	private LocalDateTime data_hora;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public String getAnexo() {
+		return anexo;
+	}
+
+	public void setAnexo(String anexo) {
+		this.anexo = anexo;
+	}
+
+	public Integer getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Integer estado) {
+		this.estado = estado;
+	}
+
+	public LocalDateTime getData_hora() {
+		return data_hora;
+	}
+
+	public void setData_hora(LocalDateTime data_hora) {
+		this.data_hora = data_hora;
+	}
+	
+	
+	
+	
 }
