68e441f181e42b373a41601106bfe221a7f03147
Victoria_Moraes <victoria.candido.mm@gmail.com>
2023-12-22 10:25:41 -0300

    add User DetailsImpL, UserDetailsImpl,Usuario Controller e Usuario repository

---
+++ src/main/java/com/generation/educajunto/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.generation.educajunto.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.educajunto.model.Usuario;
+
+public interface UsuarioRepository  extends JpaRepository<Usuario, Long>{
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
